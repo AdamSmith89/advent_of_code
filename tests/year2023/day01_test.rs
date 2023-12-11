ab139405ed02d397d18d68931262eae9dfc785c2
Adam Smith <adamsmith2708@gmail.com>
2023-12-11 16:43:15 +0000

    Tidying up day01 tests

--- tests/year2023/day01_test.rs
+++ tests/year2023/day01_test.rs
@@ -1,5 +1,6 @@
 use advent_of_code::year2023::day01::*;
 
+const PUZZLE_INPUT: &str = include_str!("../../input/year2023/day01.txt");
 const PART1_EXAMPLE: &str = "\
 1abc2
 pqr3stu8vwx
@@ -7,11 +8,19 @@ a1b2c3d4e5f
 treb7uchet";
 
 #[test]
-fn part1_test() {
-    let input = parse(PART1_EXAMPLE).expect("Failed to parse test input");
+fn part1_example() {
+    let input = parse(PART1_EXAMPLE).expect("Error parsing input");
+    let answer = part1(&input).expect("Error solving part 1");
 
-    let answer = part1(&input).expect("Failed to calculate answer");
     assert_eq!(answer, 142);
+}
+
+#[test]
+fn part1_real() {
+    let input = parse(PUZZLE_INPUT).expect("Error parsing input");
+    let answer = part1(&input).expect("Error solving part 1");
+
+    assert_eq!(answer, 54388);
 }
 
 const PART2_EXAMPLE: &str = "\
@@ -24,11 +33,19 @@ zoneight234
 7pqrstsixteen";
 
 #[test]
-fn part2_test() {
-    let input = parse(PART2_EXAMPLE).expect("Failed to parse test input");
+fn part2_example() {
+    let input = parse(PART2_EXAMPLE).expect("Error parsing input");
+    let answer = part2(&input).expect("Error solving part 2");
 
-    let answer = part2(&input).expect("Failed to calculate answer");
     assert_eq!(answer, 281);
+}
+
+#[test]
+fn part2_real() {
+    let input = parse(PUZZLE_INPUT).expect("Error parsing input");
+    let answer = part2(&input).expect("Error solving part 2");
+
+    assert_eq!(answer, 53515);
 }
 
 const TROUBLESOME: &str = "\
@@ -37,9 +54,9 @@ vfzmncfonexxkzlcstqhxvtwoplsglsix1kpkssfz
 254zhtrlvpfs";
 
 #[test]
-fn troublesome_test() {
-    let input = parse(TROUBLESOME).expect("Failed to parse test input");
+fn part2_edge_case() {
+    let input = parse(TROUBLESOME).expect("Error parsing input");
+    let answer = part2(&input).expect("Error solving part 2");
 
-    let answer = part2(&input).expect("Failed to calculate answer");
     assert_eq!(answer, 16 + 11 + 24);
 }
